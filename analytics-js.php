734340568a3ab6d84cf74938a2a0254d40b98ad9
Pablo Moretti <pablomoretti@gmail.com>
2013-10-29 00:14:08 -0300

    fix js

--- analytics-js.php
+++ analytics-js.php
@@ -2,6 +2,9 @@ header('Content-type: text/javascript');
 header('Cache-Control: maxage=43200');
 (function(window,document) {
 
-<?php=file_get_contents('http://www.google-analytics.com/analytics.js'); ?>
+<?php
+$analytics = file_get_contents('http://www.google-analytics.com/analytics.js');
+echo $analytics;
+?>
 
 })(window.parent,window.parent.document);
